3639ba19bf271b920102e0a538c2f120e4a690ec
svitlanapl <sveta_pr@i.ua>
2023-03-03 22:02:51 +0200

    added a database

---
+++ models/contact.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+
+const { handleMongooseError } = require("../helpers");
+
+const Joi = require('joi');
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
+}, { versionKey: false, timestamps: true });
+
+contactSchema.post("save", handleMongooseError);
+
+const schemaPost = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+    favorite: Joi.boolean(),
+});
+  
+const schemaPut = Joi.object({
+    name: Joi.string().optional(),
+    email: Joi.string().optional(),
+    phone: Joi.string().optional(),
+    favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+    favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+    schemaPost,
+    schemaPut,
+    updateFavoriteSchema,
+}
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+    Contact,
+    schemas,
+};
