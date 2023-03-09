2af481423bcc421c124adfc73f635954959d30c7
svitlanapl <sveta_pr@i.ua>
2023-03-09 19:46:54 +0200

    finish

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,23 +2,23 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 
-const {validateBody, isValidId} = require('../../middlewares');
+const {validateBody, isValidId, authenticate} = require('../../middlewares');
 
 const { schemas } = require('../../models/contact');
 
 const router = express.Router();
 
-router.get('/', ctrl.listContacts);
+router.get('/', authenticate, ctrl.listContacts);
 
-router.get('/:contactId', isValidId, ctrl.getContactById);
+router.get('/:contactId', authenticate, isValidId, ctrl.getContactById);
 
-router.post('/', validateBody(schemas.schemaPost), ctrl.addContact);
+router.post('/', authenticate, validateBody(schemas.schemaPost), ctrl.addContact);
 
-router.delete('/:contactId', isValidId, ctrl.removeContact);
+router.delete('/:contactId', authenticate, isValidId, ctrl.removeContact);
 
-router.put('/:contactId', isValidId, validateBody(schemas.schemaPut), ctrl.updateContact);
+router.put('/:contactId', authenticate, isValidId, validateBody(schemas.schemaPut), ctrl.updateContact);
 
-router.patch('/:contactId/favorite', isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+router.patch('/:contactId/favorite', authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
 module.exports = router;
 
